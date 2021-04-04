337e15a78ba306d60bfa60e211e58cf1f7111408
insmnia <teamspirti3209@gmail.com>
2021-04-04 10:27:59 +0300

    design

---
+++ Win32/Debug/draw2D.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import os
+with open(os.path.abspath(__file__)[:-9] + "coords.txt", "r") as coordinates:
+    x = int(coordinates.readline())
+    y = int(coordinates.readline())
+    plt.plot([0, x], [0, y], color="r")
+    plt.plot([-abs(x)*5, abs(x)*5], [0, 0], color="black", linestyle="--")
+    plt.plot([0, 0], [-abs(y)*5, abs(y)*5], color="black", linestyle="--")
+    plt.axis([-abs(x)*5, abs(x)*5, -abs(y)*5, abs(y)*5])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
---
+++ Win32/Debug/draw3D.py
@@ -0,0 +1,11 @@
+import plotly.express as px
+import os
+with open(os.path.abspath(__file__)[:-9] + "coords.txt", "r") as coords:
+    x = int(coords.readline())
+    y = int(coords.readline())
+    z = int(coords.readline())
+
+
+df = {"x": (0, x), "y": (0, y), "z": (0, z)}
+fig = px.line_3d(df, x="x", y="y", z="z")
+fig.show()
